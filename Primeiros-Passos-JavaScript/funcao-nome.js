3ea644fb6fee2faad1fafc0bd40cb86a151a1b9d
Guilherme Holler <holler.guilherme@gmail.com>
2023-11-17 02:50:14 -0300

    Add files via upload

---
+++ Primeiros-Passos-JavaScript/funcao-nome.js
@@ -0,0 +1,16 @@
+function escrevaNome (nome) {
+    console.log ('Meu nome é ' + nome);
+}
+
+escrevaNome('Guilherme');
+escrevaNome('Cristiane');
+
+function verificarIdade (idade) {
+    if (idade >= 18) {
+        console.log('Maior');
+    } else {
+        console.log ('Menor')
+    }
+}
+
+verificarIdade(31);
